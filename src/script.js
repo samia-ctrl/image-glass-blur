9f2ace2c208255f31a2bc0956ceaa44b08affe06
samia-ctrl <samiauddin99@gmail.com>
2023-08-05 21:05:39 +0100

    created function to fix content display bug
    display content + hide content functions added

--- src/script.js
+++ src/script.js
@@ -1,12 +1,16 @@
 const squareContent = document.querySelector("#square-content");
-const overlay = document.querySelector(".overlay")
-squareContent.appendChild(overlay);
+const overlay = document.querySelector(".overlay");
+overlay.style.display = "none";
 
-squareContent.addEventListener(
-  "mouseenter",
-  () => (overlay.style.display = "block")
-);
-squareContent.addEventListener(
-  "mouseleave",
-  () => (overlay.style.display = "none")
-);
+function displayContent() {
+  squareContent.appendChild(overlay);
+  overlay.style.display = "block";
+}
+
+squareContent.addEventListener("mouseenter", displayContent);
+
+function hideContent() {
+  squareContent.appendChild(overlay);
+  overlay.style.display = "none";
+}
+squareContent.addEventListener("mouseleave", hideContent);
